183dc1c89491008f2e53a023712326128d017073
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2019-11-16 13:49:23 +0100

    poprawka teksu sout

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Witaj świecie palindromów");
+        System.out.println("Witaj świecie palindromów przybyszu");
     }
 }
