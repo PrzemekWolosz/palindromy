bf950d88d4f122199f2a956b8d3b393c0e67a8ea
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2019-11-16 14:12:48 +0100

    Code do check if word is palindrom

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Witaj świecie palindromów przybyszu");
+        isPalindorm(getWord());
+    }
+
+    public static String getWord () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type a word to check if it is palindorm");
+        String word = scanner.nextLine();
+        return word;
+    }
+
+    public static void isPalindorm (String word) {
+        String front = "";
+        for (int i = 0; i < word.length() ; i++) {
+            front = front + word.charAt(i);
+        }
+        String back = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            back = back + word.charAt(i);
+        }
+        if (front.toUpperCase().equals(back.toUpperCase())) {
+            System.out.println("Palindrom");
+        } else {
+            System.out.println("Not palindorm");
+        }
     }
 }
